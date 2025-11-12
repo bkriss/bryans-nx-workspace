58bef0c2131d688ade0ed8672d4370415d6e8d24
Bryan Kriss <bkriss89@gmail.com>
2025-11-12 12:00:31 -0600

    Installed and configured Firebase Auth and Firestore

--- apps/draftkings/src/app/app.config.ts
+++ apps/draftkings/src/app/app.config.ts
@@ -4,6 +4,9 @@ import {
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -11,5 +14,21 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    // TODO: Should provideFirebaseApp initializeApp properties be moved to angular.json?
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: 'AIzaSyCVhnorDFGTiFrlMAiedPKDJpGuPL90Ats',
+        appId: '1:1010198527075:web:376bf03e9dddcb03444c56',
+        authDomain: 'dfs-lineup-builder.firebaseapp.com',
+        measurementId: 'G-QCHX9TXCJ5',
+        messagingSenderId: '1010198527075',
+        projectId: 'dfs-lineup-builder',
+        storageBucket: 'dfs-lineup-builder.firebasestorage.app',
+        // projectNumber: '1010198527075',
+        // version: '2',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
